513d5d1027b721001f97b363074e5c6e98ca091e
S.Rajesh <saluguarajesh123@gmail.com>
2025-07-24 00:00:01 +0530

    updated mobile view

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ const projects = [
   {
     title: 'Release Management Architecture',
     image: 'https://i.ibb.co/DfGfFG0x/b22ac56c-eceb-4f69-b9c7-6d896df90d47.png',
-    desc: 'Managed end-to-end SAP release cycles ensuring zero post-release defects.'
+    desc: `• Handled 200+ transport requests and system checks for go-lives.
+           • Coordinated weekly minor and monthly major releases.
+           • Reduced errors with rigorous pre-deployment validations.`
   },
   {
     title: 'SAP Monthly/Weekly Release Management',
     image: 'https://i.ibb.co/6JbGSPth/Chat-GPT-Image-Jul-19-2025-03-05-23-PM-1-page-0001.jpg',
-    desc: 'Handled 200+ transport requests and system checks for go-lives.'
+    desc: `• Handled 200+ transport requests and conducted pre-deployment validations for go-lives.
+           • Planned and coordinated weekly minor and monthly major releases, ensuring zero post-release defects.
+           • Streamlined deployment processes by automating system checks and transport tracking.`
   },
   {
     title: 'SAP Basis System Monitoring Dashboard',
-    image: 'https://i.ibb.co/DfGfFG0x/b22ac56c-eceb-4f69-b9c7-6d896df90d47.png',
-    desc: 'Developed a monitoring dashboard to track system health.'
+    image: 'Sap_Monitoring.png',
+    desc: `• Monitored system health (CPU, memory, disk) with real-time dashboards and SAP transactions like ST06.
+           • Automated job and transport monitoring, ensuring smooth deployments and proactive issue resolution.
+           • Analyzed performance trends and optimized system resources through historical and real-time data insights.`
   },
   {
     title: 'Full Stack Weather Application',
     image: 'https://i.ibb.co/wNDfjr7Q/Weather-App-Interface-Screenshots.png',
-    desc: 'Full-stack app with Spring Boot backend & Angular frontend.'
+    desc: `• Angular front-end and Spring Boot back-end.
+           • Real-time weather data using APIs.
+           • Integrated MongoDB for user data management and SQL for authentication management.`
+   
   }
 ];
 
